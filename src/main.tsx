af8d164d90647a6e4bf1ae68b007a308b30908ce
William Beaudin <williambeaudun@gmail.com>
2024-11-11 17:39:26 -0500

    feat(Change): Changed project direction as when incrementing potential ;)

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './styles/root.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </StrictMode>,
 )
--- src/view/MainView.tsx
+++ src/view/MainView.tsx
@@ -1,52 +1,18 @@
 // MainView.tsx
-import { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
-import NavbarComponent from "../components/Navbar";
+import "../styles/components/project.css";
 
 function MainView() {
-    const [showContent, setShowContent] = useState<boolean>(false);
-    const [fadeEffect, setFadeEffect] = useState<boolean>(false); // Controls fade-in and fade-out effect
-    const [lastScrollY, setLastScrollY] = useState<number>(0);
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setShowContent(true);
-            setFadeEffect(true);
-        }, 300);
-
-        return () => clearTimeout(timer);
-    }, []);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > lastScrollY) {
-                setShowContent(true);
-                setFadeEffect(true);
-            } else if (window.scrollY < lastScrollY) {
-                setFadeEffect(false);
-                setTimeout(() => setShowContent(false), 600);
-            }
-            setLastScrollY(window.scrollY);
-        };
-
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, [lastScrollY]);
-
     return (
         <>
-            {showContent && <NavbarComponent />}
-            <Container
-                fluid
-                className={`main-content text-center ${fadeEffect ? 'fade-in' : 'fade-out'}`}
-            >
+            <Container fluid className="main-content text-center">
                 <section id="home" className="mt-5">
                     <h1>Welcome to My Portfolio</h1>
                     <p>This is the main landing page of the portfolio.</p>
                 </section>
                 <section id="projects" className="mt-5">
-                    <h2>Projects</h2>
-                    <p>Explore my recent projects here.</p>
+                    <h2>About</h2>
+                    <p>A brief introduction about myself and my work.</p>
                 </section>
                 <section id="about" className="mt-5">
                     <h2>About</h2>
